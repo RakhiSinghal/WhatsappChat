4924f37660a211c7f90412ece62c23cb08e29b86
RakhiSinghal <rakhi3966singhal@gmail.com>
2024-05-29 17:32:56 +0530

    Initial Commit

---
+++ WhatsappChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsappChat
+//
+//  Created by Rakhi Singhal on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsappChat/WhatsappChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappChatApp.swift
+//  WhatsappChat
+//
+//  Created by Rakhi Singhal on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
